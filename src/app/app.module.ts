fa653424355852dfefdffa53b367977909281a40
Abel Adane <contacttoabel@gmail.com>
2023-04-07 17:06:44 +0300

    added tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DropDownComponent } from './pages/drop-down/drop-down.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, DropDownComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/drop-down/drop-down.component.ts
@@ -0,0 +1,11 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'drop-down',
+  templateUrl: './drop-down.component.html',
+  styleUrls: ['./drop-down.component.scss']
+})
+export class DropDownComponent {
+  title = 'drop-down';
+}
